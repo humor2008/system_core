DarkNess-reDefined/android_system_core

fs_mgr: zram: allow specifying stream count
This parameter allows specifying the number of parallel streams
to be used in compression and decompression. This option was added
in Linux 3.15, so it is not very common among devices currently on
the market, but it has been backported to some kernels. This option
must be set before zRAM is activated, as the kernel does not allow
switching from single stream to multistream compression once zRAM
is active.

Change-Id: I3f5ad96a65b3b4a6915d2700c398a236ea8931b0

fs_mgr/fs_mgr_priv.h
@@ -85,6 +85,7 @@ __BEGIN_DECLS
 #define MF_FORCEFDEORFBE 0x10000
 #define MF_LATEMOUNT    0x20000
 #define MF_NOFAIL       0x40000
+#define MF_ZRAMSTREAMS  0x80000
 
 #define DM_BUF_SIZE 4096
 

humor2008/system_core

fs_mgr/fs_mgr_priv.h
@@ -76,6 +76,7 @@
 #define MF_NOEMULATEDSD 0x800 /* no emulated sdcard daemon, sd card is the only
                                  external storage */
 #define MF_FORMATTABLE  0x1000
+#define MF_ZRAMSTREAMS  0x2000
 
 #define DM_BUF_SIZE 4096
 